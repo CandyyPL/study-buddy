60b7d96529e13c46698a686729d3819460d31093
CandyyPL <aplay58.yt@gmail.com>
2022-05-01 17:48:19 +0200

    Added useCallback to useStudents

--- src/hooks/useStudents.js
+++ src/hooks/useStudents.js
@@ -1,23 +1,24 @@
 import axios from 'axios'
+import { useCallback } from 'react'
 
 export const useStudents = () => {
-  const getGroups = async () => {
+  const getGroups = useCallback(async () => {
     try {
       const result = await axios.get('/groups')
       return result.data
     } catch (e) {
       console.log(e)
     }
-  }
+  }, [])
 
-  const getStudents = async (groupId = '') => {
+  const getStudents = useCallback(async (groupId = '') => {
     try {
       const result = axios.get(`/students${groupId ? `?group=${groupId}` : ''}`)
       return result
     } catch (e) {
       console.log(e)
     }
-  }
+  }, [])
 
   return { getGroups, getStudents }
 }
